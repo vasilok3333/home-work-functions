cfe3caf3740b87bc22a36f00850615445c3f7ede
Vasilok3333 <vasylshchur3333@gmail.com>
2021-02-26 00:11:28 +0200

    added files

---
+++ main.js
@@ -0,0 +1,104 @@
+//Task 1
+//Напишіть функцію, що повертає куб числа.
+
+let getCubeNumber = (number) => Math.pow(number, 3)
+
+let cubeNumber = getCubeNumber(4);
+
+console.log(cubeNumber);
+
+
+//Task 2 
+//Напишіть функцію, що додає перше число до другого і ділить результат на третє число.
+
+function getNumberOfMath(first, second, third) {
+    return (first + second) / third;
+    s
+}
+
+let number = getNumberOfMath(3, 4, 7);
+
+console.log(number);
+
+//Task 3 
+//Напишіть функцію, що приймає число від 1 до 7 і повертає відповідну назву дня (українською).
+let nameDay = getCurrentNameDay(4);
+
+function getCurrentNameDay(num) {
+    switch (num) {
+        case 1:
+            day = 'Понеділок';
+            break;
+        case 2:
+            day = 'Вівторок';
+            break;
+        case 3:
+            day = 'Середа';
+            break;
+        case 4:
+            day = 'Четвер';
+            break;
+        case 5:
+            day = 'Пятниця';
+            break;
+        case 6:
+            day = 'Суботу';
+            break;
+        case 7:
+            day = 'Неділя';
+            break;
+        default:
+            day = 'Введіть число від 1 до 7';
+
+    }
+    return day;
+}
+
+console.log(nameDay);
+
+
+//Task4 
+//Реалізуйте функцію знаходження факторіала
+
+function getFactorial(num) {
+    let factorial = num;
+    while (num != 1) {
+        factorial = factorial * (num - 1);
+        num--;
+    }
+
+    return factorial;
+
+}
+
+console.log(getFactorial(5));
+
+// або рекурсія
+
+function getFactorial2(num) {
+    if (num != 1) {
+        return num * getFactorial2(num - 1)
+    } else {
+        return 1;
+    }
+}
+
+console.log(getFactorial2(5));
+
+
+//Task 5 
+//5.  Напишіть функцію яка отримує години хвилини та секунди і повертає це число в секундах.
+const SECONDS_IN_MINUTE = 60;
+const SECONDS_IN_HOUR = 3600;
+
+
+function getSeconds(hours, min, sec) {
+    return hours * SECONDS_IN_HOUR + min * SECONDS_IN_MINUTE + sec;
+}
+let sumSeconds = getSeconds(3, 45, 46);
+
+console.log(sumSeconds);
+
+
+//Task 6 
+// Написати функцію , яка приймає секунди, і перетворює їх у години хвилини та секунди у форматі «гг:хх:сс». якщо кількість годин більша за 23.59.59 - вивести повідомлення "Більше одного дня".
